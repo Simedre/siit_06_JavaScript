7e8c74e493564a6ad667b6c140caa70687186511
Simedre <adrian.simedre@gmail.com>
2019-06-20 04:58:20 +0300

    conectare cu baza de date

--- Shopping_List/script.js
+++ Shopping_List/script.js
@@ -1,6 +1,9 @@
 var shoppingList = [];
+var shoppingListArr = [];
 var index2;
 var dir;
+var url = "https://shopping-list-3f532.firebaseio.com/.json";
+var indexEdit = -1;
 
 class Item {
     constructor(denumire, mark){
@@ -9,28 +12,55 @@ class Item {
     }
 }
 
-function addItem(event){
+async function ajaxPromise(url,method, body){
+    return new Promise(function(resolve,reject){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if ( this.readyState == 4 ){
+                if ( this.status == 200 ){
+                    resolve(this.responseText)
+                } else {
+                    reject(this);
+                }
+            }
+        };
+        xhttp.open(method, url, true);
+        xhttp.send(body);
+    });
+}
+
+async function addItem(event){
     event.preventDefault();
     if (event.target.id == "shopping_submit"){
         var product = document.getElementById("product").value;
         if (product){
-            var temp = new Item(product);
-            shoppingList.push(temp)
+            var temp ={
+                denumire : product,
+                mark : false
+            };
+            await ajaxPromise( "https://shopping-list-3f532.firebaseio.com/.json","POST",JSON.stringify(temp));
             document.getElementById("product").value = "";
         }
     }
-    //console.log(shoppingList);
 }
 document.getElementById("shopping_form").addEventListener("click", addItem);
 
+async function getLista(){
+    var responseText = await ajaxPromise("https://shopping-list-3f532.firebaseio.com/.json","GET");
+    window.shoppingList = JSON.parse(responseText);
+    window.shoppingListArr = Object.keys(shoppingList).map(function(key) {
+        return [Number(key), shoppingList[key]];
+      });
+    drawList();
+}
+
 function drawList(){
-    if (event.target.id == "shopping_submit"){
+    if (event.target.id == "shopping_submit" || shoppingListArr.length > 0){
         var show = document.getElementById("show_shoppind");
         show.className = " show";
     }
     var tableShopping = document.getElementById("table_shopping");
     var tableDataShopping = "";
-
     var table = document.getElementById("table_shopping");
     var rows = table.rows;
     if (event.target.id == "shopping_submit" && Number(rows.length) > 1){
@@ -38,80 +68,91 @@ function drawList(){
         var newRow = tableShopping.insertRow(nrRow);
         var x = newRow.insertCell(0);
         var y = newRow.insertCell(1);
-        x.innerHTML = `${shoppingList[shoppingList.length-1].denumire}`;
+        var nr = shoppingListArr.length
+        x.innerHTML = `${shoppingListArr[nr-1][1].denumire}`;
         y.innerHTML = `Mark as buyed`;
         y.setAttribute("id", "mark");
-    } else if (event.target.id == "shopping_submit" || event.target.id == "asc" || event.target.id == "dsc"){
-    var tableHeadShopping = `<tr>
+    } else {
+        var tableHeadShopping = `<tr>
                                 <th>Item description</th>
                                 <th>Action</th>
-                            </tr>`;
-    for (var i = 0; i < shoppingList.length; i++){
-        if (shoppingList[i].mark == true){
-            tableDataShopping += `<tr>
+                                <th>Delete</th>
+                                </tr>`;
+        for (var i in shoppingList){
+            if (shoppingList[i].mark == true){
+                tableDataShopping += `<tr>
                                         <td class="marked">${shoppingList[i].denumire}</td>
                                         <td id="mark">Mark as buyed</td>
-                                </tr>`;
-        } else {
-            tableDataShopping += `<tr>
-                                    <td>${shoppingList[i].denumire}</td>
-                                    <td id="mark">Mark as buyed</td>
-                                </tr>`;
+                                        <td class="dellBtn" onclick="del('${i}')"><td>
+                                    </tr>`;
+            } else {
+                tableDataShopping += `<tr>
+                                        <td>${shoppingList[i].denumire}</td>
+                                        <td id="mark">Mark as buyed</td>
+                                        <td class="dellBtn" onclick="del('${i}')"><td>
+                                    </tr>`;
+            }
         }
-    }
     tableShopping.innerHTML = tableHeadShopping + tableDataShopping;
-}
-//console.log(Number(rows.length));
+    }
 }
 document.getElementById("shopping_form").addEventListener("click", drawList);
 
-function markBuyed(){
+async function markBuyed(){
     index2 = Number(event.target.parentElement.rowIndex - 1);
     if (event.target.id == "mark"){
         var table = document.getElementById("table_shopping");
         var rows = table.rows;
-        shoppingList[index2].mark = true;
         var cell = rows[index2+1].cells[0];
         cell.className = "marked";
+        var keys = Object. keys(shoppingList) ;
+        indexEdit = keys[index2];
+        var temp = {
+            denumire : shoppingListArr[index2][1].denumire,
+            mark : true
+        }
+            await ajaxPromise(
+                `https://shopping-list-3f532.firebaseio.com/${indexEdit}.json`,
+                "PUT",
+                JSON.stringify(temp));
     }
-    //console.log(shoppingList);
-    //console.log(rows);
-    //console.log(index2);
-    //console.log(cell);
 }
 document.getElementById("table_shopping").addEventListener("click", markBuyed);
 
-function sort(shoppingList, dir){
-    for (var i = 0; i < shoppingList.length; i++){
-        for (var j = i + 1; j < shoppingList.length; j++){
+async function sort(shoppingListArr, dir){
+    for (var i = 0; i < shoppingListArr.length; i++){
+        for (var j = i + 1; j < shoppingListArr.length; j++){
             if (dir === "dsc"){
-                if (shoppingList[i].denumire > shoppingList[j].denumire){
-                    var temp = shoppingList[i];
-                    shoppingList[i] = shoppingList[j];
-                    shoppingList[j] = temp;
+                if (shoppingListArr[i][1].denumire > shoppingListArr[j][1].denumire){
+                    var temp = shoppingListArr[i];
+                    shoppingListArr[i] = shoppingListArr[j];
+                    shoppingListArr[j] = temp;
                 }
             } else if (dir === "asc"){
-                if (shoppingList[i].denumire < shoppingList[j].denumire){
-                    var temp = shoppingList[i];
-                    shoppingList[i] = shoppingList[j];
-                    shoppingList[j] = temp; 
+                if (shoppingListArr[i][1].denumire < shoppingListArr[j][1].denumire){
+                    var temp = shoppingListArr[i];
+                    shoppingListArr[i] = shoppingListArr[j];
+                    shoppingListArr[j] = temp; 
                 }
             }
         }
     }
-    drawList(shoppingList);
+    drawList();
 }
 
 function criteriu(){
         dir = event.target.id;
         if (dir == "asc"){
             dir = "dsc";
-            culoareColoana1 = "green";
         } else{
             dir = "asc";
-            culoareColoana1 = "red";
         }
         sort(shoppingList, dir);
 }
 document.getElementById("asc").addEventListener("click", criteriu);
 document.getElementById("dsc").addEventListener("click", criteriu);
+
+async function del(idx){
+    await ajaxPromise(`https://shopping-list-3f532.firebaseio.com/${idx}.json`,"DELETE");
+    getLista();
+}
